275829b5004753b7dfbdac709efbded004f03da9
Ryan Sun <ryanrsun@gmail.com>
2017-10-09 18:15:52 -0400

    First commit

---
+++ R/PCs_1000G_Euro-data.R
@@ -0,0 +1,14 @@
+#' PCs for 1000G population of 350 European subjects
+#'
+#' @docType data
+#'
+#' @usage data(PCs_1000G_Euro)
+#'
+#' @format A table with first column as ID and rest of columns as PC1-PC20
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(PCs_1000G_Euro)
+#' print(PCs_1000G_Euro[1, ])
+"PCs_1000G_Euro"
---
+++ R/bader_apr1_pathways-data.R
@@ -0,0 +1,14 @@
+#' Pathway table saved from 'pathways_to_test_062817_sorte.txt
+#'
+#' @docType data
+#'
+#' @usage data(bader_apr1_pathways)
+#'
+#' @format A data table with the transcription start/stop point for >48,000 genes.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(bader_apr1_pathways)
+#' print(bader_apr1_pathways[1, ])
+"bader_apr1_pathways"
