ceda25c1144488e80b24b170d0f81f584bbdb137
Ryan Sun <ryanrsun@gmail.com>
2017-10-09 18:45:59 -0400

    Now calling it ensembl_hg19_oct17 instead of gene_info

--- R/gene_info-data.R
+++ R/gene_info-data.R
@@ -1,17 +1,23 @@
 #' Hg19 Gene table using mostly Ensembl annotations
 #'
 #' See make_ensembl_gene_list.R for information on how we created this table.
+#' There are a small number (17) of genes with N/A chromosome - that's just how
+#' they came to us from refgene (and were not in ensembl).
+#' A '2' in the Notes column means it is a duplicate entry of another gene with the
+#' same HGNC name - sometimes refgene puts the same gene on multiple chromosomes
+#' or in multiple bands along the same chromosome.  There will be another entry with
+#' the same HGNC name and a '1' in the Notes column, this is the primary entry.
 #'
 #' @docType data
 #'
-#' @usage data(gene_info)
+#' @usage data(ensembl_hg19_oct17)
 #'
 #' @format A data table with the transcription start/stop point for >48,000 genes.
 #'
 #' @keywords datasets
 #'
 #' @examples
-#' data(gene_info)
-#' FGFR2_row <- which(gene_info$HGNC_name == 'FGFR2')
-#' print(gene_info[FGFR2_row, ])
-"gene_info"
+#' data(ensembl_hg19_oct17)
+#' FGFR2_row <- which(ensembl_hg19_oct17$HGNC_name == 'FGFR2')
+#' print(ensembl_hg19_oct17[FGFR2_row, ])
+"ensembl_hg19_oct17"
